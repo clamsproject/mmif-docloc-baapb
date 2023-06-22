5e7cefbe3a262df5cb4ffad52016fe77fa6ff7fc
leorabaumgarten <baumgartenleora@gmail.com>
2023-06-22 14:10:24 -0400

    corrected __init__.py to work with server

--- mmif_docloc_baapb/__init__.py
+++ mmif_docloc_baapb/__init__.py
@@ -1,18 +1,18 @@
+import requests
+import os
 
-# TODO: this value should be configurable via environment variable
-RESOVLER_ADDRESS='localhost:20202'
+if 'RESOLVER_ADDRESS' in os.environ:
+    RESOLVER_ADDRESS = os.environ['RESOLVER_ADDRESS']
+else:
+    RESOLVER_ADDRESS='localhost:5000'
 
 def resolve(docloc):
     scheme = 'baapb'
     if docloc.startswith(f'{scheme}://'):
         guid, document_type = docloc[len(scheme)+3:].rsplit('.', 1)
-        # TODO: for now, just return a dummy file path
-        if 'video' in document_type.lower():
-            return '/path/to/cpb-aacip_15-99p2w600.mp4'
-        elif 'audio' in document_type.lower():
-            return '/path/to/cpb-aacip_15-99p2w600.mp3'
-        elif 'text' in document_type.lower():
-            return '/path/to/cpb-aacip_15-99p2w600.txt'
+        url = 'http://' + RESOLVER_ADDRESS + '/searchapi'
+        r = requests.get(url, params={'guid': guid, 'file':document_type})
+        return r.text
     else:
         raise ValueError(f'cannot handle document location scheme: {docloc}')
 
